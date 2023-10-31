b534bc90d374116cb08199bf0da8813fa86e3306
SaulDiazInfante <=>
2023-10-30 17:05:59 -0700

    precommit

---
+++ tests/mod_milstein_solver_test.f90.d
@@ -0,0 +1,3 @@
+$(tests/mod_milstein_solver_test.f90) += $(mod_milstein_solver.mod)
+$(tests/mod_milstein_solver_test.f90) += $(mod_stochastic_logistic_model.mod)
+$(tests/mod_milstein_solver_test.f90) += $(mod_random_number_generator.mod)
